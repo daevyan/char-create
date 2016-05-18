f17cce160a36e8504d6d0e85ebb30825c2d7f6e8
Marcin Malczewski <marcin.malczewski@siili.fi>
2016-05-18 16:21:36 +0200

    Create separate classes for int and list

--- main.py
+++ main.py
@@ -9,7 +9,82 @@ class CharCreator(object):
         print "Welcome to the Character Creation Tool for handicapped people like me.\n"
 
 
-class Console(object):
+class ConsoleInt(object):
+    def __init__(self):
+        self.answer_range = range(20, 150)
+        self.range_dict = {
+            "a kid": range(5, 11),
+            "a teen": range(11, 18),
+            "a young adult": range(18, 26),
+            "an adult": range(26, 40),
+            "middle aged": range(40, 66),
+            "elderly": range(66, 121)
+        }
+
+    def console_int(self, answer, data_type):  # Check if answer in range
+        def check_if_int():
+            if data_type is "number":
+                print "It should be a number!"
+                while True:
+                    try:
+                        self.console_cmd(answer)
+                        int(answer)
+                    except ValueError:
+                        print("That's either not a whole number or not a number at all! Try again.")
+                        # answer = raw_input(">")
+                    else:
+                        return int(answer), True
+            else:
+                return answer, False
+
+        def check_if_in_range(int_answer):
+            if int_answer in self.answer_range:
+                if self.range_dict is not None:
+                    get_range_name(int_answer, self.range_dict)
+                else:
+                    return int_answer
+            else:
+                print "This value is not in range. Range is %s to %s." \
+                      % (self.answer_range[0], self.answer_range[-1])
+                new_int_answer = check_if_int(raw_input(">"), data_type)
+                check_if_in_range(new_int_answer)
+
+        def get_range_name(int_answer, range_dict):
+            for range_name, range_value in range_dict.iteritems():
+                if int_answer in range_value:
+                    return int_answer, range_name
+
+        int_answer, is_int = check_if_int()
+        if is_int and self.answer_range is not None:
+            return check_if_in_range(int_answer)
+        else:
+            pass
+
+    def input_int(self):
+        new_value = int(raw_input(">"))
+        return new_value
+
+
+class ConsoleList(object):
+    def __init__(self):
+        pass
+
+    def console_list(self, answer, finite=False, listname=None):
+        while finite:
+            if answer in listname:
+                return answer
+            else:
+                print "Your answer is not valid. Type 'help' for available answers"
+                answer = raw_input(">")
+
+    def check_if_list(self, data_type=None):
+        if data_type is "list":
+            return True
+        else:
+            return False
+
+
+class ConsoleCommand(object):
 
     def __init__(self):
         self.commands = {
@@ -56,7 +131,7 @@ class Console(object):
                                         # Returns answer
         command_name, is_cmd = self.check_if_cmd(answer)
         if is_cmd:
-            self.run_command(command_name)
+            return self.run_command(command_name)
         else:
             return answer
 
@@ -72,65 +147,9 @@ class Console(object):
         print "Not a command"
         return answer, False
 
-    def console_int(self, answer, data_type, answer_range=None, range_dict=None):  # Check if answer in range
-        def check_if_int():
-            if data_type is "number":
-                print "It should be a number!"
-                while True:
-                    try:
-                        self.console_cmd(answer)
-                        int(answer)
-                    except ValueError:
-                        print("That's either not a whole number or not a number at all! Try again.")
-                        # answer = raw_input(">")
-                    else:
-                        return int(answer), True
-            else:
-                return answer, False
-
-        def check_if_in_range(int_answer):
-            if int_answer in answer_range:
-                if range_dict is not None:
-                    get_range_name(int_answer, range_dict)
-                else:
-                    return int_answer
-            else:
-                print "This value is not in range. Range is %s to %s."\
-                      % (answer_range[0], answer_range[-1])
-                new_int_answer = check_if_int(raw_input(">"), data_type)
-                check_if_in_range(new_int_answer)
-
-        def get_range_name(int_answer, range_dict):
-            for range_name, range_value in range_dict.iteritems():
-                if int_answer in range_value:
-                    return int_answer, range_name
-
-        int_answer, is_int = check_if_int()
-        if is_int and answer_range is not None:
-            return check_if_in_range(int_answer)
-        else:
-            pass
-
-    def input_int(self):
-        new_value = int(raw_input(">"))
-        return new_value
 
 
 
-
-    def console_list(self, answer, finite=False, listname=None):
-        while finite:
-            if answer in listname:
-                return answer
-            else:
-                print "Your answer is not valid. Type 'help' for available answers"
-                answer = raw_input(">")
-
-    def check_if_list(self, data_type=None):
-        if data_type is "list":
-            return True
-        else:
-            return False
 
     def check_answer_type(self):
         pass
@@ -149,18 +168,10 @@ class Console(object):
 # a_list = ['one', 'two', 'gods', "why"]
 le_answer = raw_input(">")
 # le_answer = raw_input(">")
-b = Console()
+b = ConsoleCommand()
 # b.run_command(le_answer, "list", a_list, True)
-le_answer_range = range(20, 150)
-le_range_dict = {
-    "a kid": range(5, 11),
-    "a teen": range(11, 18),
-    "a young adult": range(18, 26),
-    "an adult": range(26, 40),
-    "middle aged": range(40, 66),
-    "elderly": range(66, 121)
-}
-thing = b.console_int(le_answer, "number", le_answer_range)
+
+thing = b.console_int(le_answer, "number")
 print thing
 
 # if le_answer in le_answer_range:
